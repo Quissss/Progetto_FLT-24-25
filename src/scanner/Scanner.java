af9b3075c39786d66f00a1ed43b2e626bc7790e8
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-12-07 22:47:44 +0100

    Work in Progress Scanner
    Messo javadoc e fatto modifiche

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -10,32 +10,44 @@ import java.util.HashSet;
 import exception.LexicalException;
 import token.*;
 
+
+
+/**
+ * La classe Scanner si occupa di scansionare un file di input carattere per carattere
+ * per generare i relativi token che saranno utilizzati da un parser. Gli operatori,
+ * le parole chiave, gli identificatori e i numeri vengono riconosciuti e restituiti
+ * come token corrispondenti.
+ */
 public class Scanner {
 
 	final char EOF = (char) -1;
 	private int riga;
 	private PushbackReader buffer;
-	private String log;
 	private Token current;
-	
-	/** insieme di caratteri non considerati dallo scanner (incluso EOF) */
-	public HashSet<Character> skpChars; 
-	/** insieme di lettere dell'alfabeto */
-	public HashSet<Character> letters; 
-	/** insieme di cifre */
-	public HashSet<Character> digits; 
 
-	
+	/** insieme di caratteri non considerati dallo scanner (incluso EOF) */
+	public HashSet<Character> skpChars;
+	/** insieme di lettere dell'alfabeto */
+	public HashSet<Character> letters;
+	/** insieme di cifre */
+	public HashSet<Character> digits;
+
 	/** mapping fra caratteri '+', '-', '*', '/' e il TokenType corrispondente */
-	private HashMap<Character, TokenType> operTkType; 
+	private HashMap<Character, TokenType> operTkType;
 	/** mapping fra caratteri '=', ';' e il e il TokenType corrispondente */
-	private HashMap<Character, TokenType> delimTkType; 
-	/** mapping fra le stringhe "print", "float", "int" e il TokenType corrispondente */
-	private HashMap<String, TokenType> keyWordsTkType; 
+	private HashMap<Character, TokenType> delimTkType;
+	/**
+	 * mapping fra le stringhe "print", "float", "int" e il TokenType corrispondente
+	 */
+	private HashMap<String, TokenType> keyWordsTkType;
 
 	/**
-	 * Inizializza il costruttore
-	 */
+     * Costruttore della classe Scanner. Inizializza il lettore del file e configura
+     * i set e le mappe necessarie per il riconoscimento dei token.
+     * 
+     * @param fileName il nome del file da scansionare
+     * @throws FileNotFoundException se il file non può essere trovato
+     */
 	public Scanner(String fileName) throws FileNotFoundException {
 
 		this.buffer = new PushbackReader(new FileReader(fileName));
@@ -47,8 +59,10 @@ public class Scanner {
 	}
 
 	/**
-	 * Inizializza i campi dello scanner.
-	 */
+     * Inizializza i set e le mappe per lo scanner. Definisce i caratteri da saltare,
+     * i caratteri alfabetici e numerici, i token per gli operatori, i delimitatori e 
+     * le parole chiave.
+     */
 	private void config() {
 		/* Inizializza il set skpChars */
 		skpChars = new HashSet<Character>();
@@ -89,6 +103,15 @@ public class Scanner {
 		keyWordsTkType.put("int", TokenType.TYINT);
 	}
 
+	/**
+     * Ritorna il prossimo token nel file di input, scansionando il file carattere per carattere.
+     * Gestisce gli spazi, le parole chiave, gli identificatori, gli operatori, i numeri
+     * e i delimitatori.
+     * 
+     * @return il prossimo token trovato
+     * @throws LexicalException se il token non è valido
+     * @throws IOException se si verifica un errore di I/O durante la lettura del file
+     */
 	public Token nextToken() throws LexicalException, IOException {
 
 		if (current != null) {
@@ -96,13 +119,9 @@ public class Scanner {
 			current = null;
 			return token;
 		}
-		char nextChar;
-		nextChar = peekChar();
+		char nextChar = peekChar();
 
-		// Avanza nel buffer leggendo i carattere in skipChars
-		// incrementando riga se leggi '\n'.
-		// Se raggiungi la fine del file ritorna il Token EOF
-		// Salta i caratteri di spazio e di fine linea
+		 // Salta i caratteri non rilevanti (spazi, tabulazioni, ritorni a capo)
 		while (skpChars.contains(nextChar)) {
 			if (nextChar == '\n') {
 				riga++; // Incrementa la riga se trovi un ritorno a capo
@@ -114,93 +133,125 @@ public class Scanner {
 			return new Token(TokenType.EOF, riga);
 		}
 
-		// Se nextChar e' in letters
-		// return scanId()
-		// che deve generare o un Token ID o parola chiave
-		// Se il carattere è una lettera (ID o parola chiave)
+		// Scansiona gli identificatori o le parole chiave
 		if (letters.contains(nextChar)) {
-			return scanId(nextChar);
+			return scanId();
+		}
+		
+		// Scansiona gli operatori
+		if (operTkType.containsKey(nextChar)) {
+			return scanOperator();
 		}
 
-		// Se nextChar e' o in operators oppure delimitatore
-		// ritorna il Token associato con l'operatore o il delimitatore
-		// Attenzione agli operatori di assegnamento!
-		// Se il carattere è un operatore o delimitatore
-		// Se nextChar è un operatore o un delimitatore
-		// Se nextChar è un operatore o un delimitatore
-	    if (operTkType.containsKey(nextChar)) {
-	        return new Token(operTkType.get(nextChar), riga); // Restituisce il token associato all'operatore
-	    }
+		if (delimTkType.containsKey(nextChar)) {
+			return new Token(delimTkType.get(nextChar), riga); // Restituisce il token associato al delimitatore
+		}
 
-	    if (delimTkType.containsKey(nextChar)) {
-	        return new Token(delimTkType.get(nextChar), riga); // Restituisce il token associato al delimitatore
-	    }
-
-		// Se nextChar e' in numbers
-		// return scanNumber()
-		// che legge sia un intero che un float e ritorna il Token INUM o FNUM
-		// i caratteri che leggete devono essere accumulati in una stringa
-		// che verra' assegnata al campo valore del Token
-		// Se il carattere è un numero (intero o float)
+		// Scansiona i numeri (interi o decimali)
 		if (digits.contains(nextChar)) {
-			return scanNumber(nextChar);
+			return scanNumber();
 		}
-
-		// Altrimenti il carattere NON E' UN CARATTERE LEGALE sollevate una
-		// eccezione lessicale dicendo la riga e il carattere che la hanno
-		// provocata.
 
 		throw new LexicalException(riga, nextChar);
 	}
 
-	private Token scanId(char firstChar) throws IOException {
-	    StringBuilder idValue = new StringBuilder();
-	    idValue.append(firstChar);
+	
+	/**
+     * Scansiona un identificatore o una parola chiave.
+     * Un identificatore può essere una parola chiave (come "int", "float", "print")
+     * o un identificatore generico.
+     * 
+     * @return il token identificatore o parola chiave
+     * @throws IOException se si verifica un errore di lettura
+     */
+	private Token scanId() throws IOException {
+		StringBuilder idValue = new StringBuilder();
+		char nextChar = readChar();
+		idValue.append(nextChar);
 
-	    char nextChar = readChar();
-	    while (letters.contains(nextChar) || digits.contains(nextChar)) {
-	        idValue.append(nextChar);
-	        nextChar = readChar();
+		nextChar = readChar();
+		while (letters.contains(nextChar) || digits.contains(nextChar)) {
+			idValue.append(nextChar);
+			nextChar = readChar();
+		}
+
+		buffer.unread(nextChar);
+		String id = idValue.toString();
+		if (keyWordsTkType.containsKey(id)) {
+			return new Token(keyWordsTkType.get(id), riga, id); // Parola chiave
+		} else {
+			return new Token(TokenType.ID, riga, id); // Identificatore generico
+		}
+	}
+	
+	/**
+     * Scansiona un operatore. Gli operatori riconosciuti includono +, -, *, / e ":=".
+     * 
+     * @return il token dell'operatore trovato
+     * @throws IOException se si verifica un errore di lettura
+     * @throws LexicalException se l'operatore non è valido
+     */
+	private Token scanOperator() throws IOException, LexicalException {
+	    char opChar = readChar(); // Leggi il carattere dell'operatore
+
+	    // Se è un operatore base come +, -, *, /, ritorna il relativo token
+	    TokenType type = operTkType.get(opChar);
+	    if (type != null) {
+	        return new Token(type, riga);
 	    }
 
-	    // Se la stringa è una parola chiave, ritorna il tipo corrispondente
-	    String id = idValue.toString();
-	    if (keyWordsTkType.containsKey(id)) {
-	        return new Token(keyWordsTkType.get(id), riga, id);
-	    } else {
-	        return new Token(TokenType.ID, riga, id);
+	    // Gestione dell'operatore di assegnamento ":=" (esempio di operatore composto)
+	    if (opChar == ':' && peekChar() == '=') {
+	        readChar(); // Leggi il secondo carattere '='
+	        return new Token(TokenType.OP_ASSIGN, riga);
 	    }
+
+	    // Se il carattere non è un operatore valido, lancia un'eccezione
+	    throw new LexicalException(riga, opChar);
 	}
 
-	private Token scanOperator(char nextChar) throws LexicalException {
-	    // Se il carattere è un operatore
-	    
-	    throw new LexicalException(riga, nextChar);
-	}
 
-	private Token scanNumber(char firstChar) throws IOException {
+	/**
+     * Scansiona un numero intero o decimale.
+     * I numeri possono essere interi (es. 123) o decimali (es. 12.34).
+     * 
+     * @return il token del numero trovato
+     * @throws IOException se si verifica un errore di lettura
+     * @throws LexicalException se il numero non è valido
+     */
+	private Token scanNumber() throws IOException, LexicalException {
 	    StringBuilder numberValue = new StringBuilder();
-	    numberValue.append(firstChar);
-
 	    char nextChar = readChar();
-	    boolean isFloat = false;
+	    numberValue.append(nextChar);
 
-	    // Legge i numeri interi o i numeri con decimali
+	    boolean isFloat = false;
+	    nextChar = readChar();
+
+	    // Continua a leggere caratteri numerici o un punto per decimali
 	    while (digits.contains(nextChar) || (nextChar == '.' && !isFloat)) {
 	        if (nextChar == '.') {
-	            isFloat = true;
+	            isFloat = true; // Flag per numeri decimali
 	        }
 	        numberValue.append(nextChar);
 	        nextChar = readChar();
 	    }
 
-	    // Restituisci un token di tipo appropriato (INT o FLOAT)
-	    if (isFloat) {
-	        return new Token(TokenType.FLOAT, riga, numberValue.toString());
-	    } else {
-	        return new Token(TokenType.INT, riga, numberValue.toString());
+	    buffer.unread(nextChar);
+
+	    String numberStr = numberValue.toString();
+	    try {
+	        if (isFloat) {
+	            Float.parseFloat(numberStr);
+	            return new Token(TokenType.FLOAT, riga, numberStr); // TokenType.FLOAT per numeri decimali
+	        } else {
+	            Integer.parseInt(numberStr);
+	            return new Token(TokenType.INT, riga, numberStr); // TokenType.INT per numeri interi
+	        }
+	    } catch (NumberFormatException e) {
+	        throw new LexicalException(riga, numberStr); // Se la conversione fallisce
 	    }
 	}
+
 
 	/**
 	 * Legge il prossimo carattere dal buffer di input.
