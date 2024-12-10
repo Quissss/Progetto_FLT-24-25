6e9f1931b80826bf64639195a260e152909d8942
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-12-10 01:09:50 +0100

    Aggiunti test su tokenType

--- src/test/TestToken.java
+++ src/test/TestToken.java
@@ -9,37 +9,61 @@ import token.TokenType;
 
 class TestToken {
 
-	@Test
-	void testCostruttore() {
-		Token tk1 = new Token(TokenType.SEMI, 1);
-		Token tk2 = new Token(TokenType.PLUS, 5);
-		
-		assertEquals(tk1.getType(), TokenType.SEMI);
-		assertEquals(tk1.getRow(), 1);
-		assertEquals(tk2.getType(), TokenType.PLUS);
-		assertEquals(tk2.getRow(), 5);
-	}
-	
-	@Test
-	void testCostruttoreValore() {
-		Token tk1 = new Token(TokenType.INT, 4, "19");
-		Token tk2 = new Token(TokenType.ID, 2, "b");
-		
-		assertEquals(tk1.getType(), TokenType.INT);
-		assertEquals(tk1.getRow(), 4);
-		assertEquals(tk1.getValue(), "19");
-		assertEquals(tk2.getType(), TokenType.ID);
-		assertEquals(tk2.getRow(), 2);
-		assertEquals(tk2.getValue(), "b");
-	}
-	
-	@Test
-	void testToString() {
-		Token tk1 = new Token(TokenType.SEMI, 1);
-		Token tk2 = new Token(TokenType.ID, 2, "b");
-		
-		assertEquals(tk1.toString(), "SEMI, riga: 1");
-		assertEquals(tk2.toString(), "ID, riga: 2, b");
-	}
+	 @Test
+	    void testCostruttore() {
+	        Token token1 = new Token(TokenType.SEMI, 1);
+	        Token token2 = new Token(TokenType.PLUS, 5);
+
+	        assertEquals(TokenType.SEMI, token1.getType());
+	        assertEquals(1, token1.getRow());
+	        assertEquals(TokenType.PLUS, token2.getType());
+	        assertEquals(5, token2.getRow());
+	    }
+
+	    @Test
+	    void testCostruttoreValore() {
+	        Token token1 = new Token(TokenType.INT, 5, "21");
+	        Token token2 = new Token(TokenType.ID, 3, "z");
+
+	        assertEquals(TokenType.INT, token1.getType());
+	        assertEquals(5, token1.getRow());
+	        assertEquals("21", token1.getValue());
+	        assertEquals(TokenType.ID, token2.getType());
+	        assertEquals(3, token2.getRow());
+	        assertEquals("z", token2.getValue());
+	    }
+
+	    @Test
+	    void testSetType() {
+	        Token tk = new Token(TokenType.MINUS, 4);
+	        assertEquals(TokenType.MINUS, tk.getType());
+
+	        tk.setType(TokenType.PLUS);
+	        assertEquals(TokenType.PLUS, tk.getType());
+	    }
+
+	    @Test
+	    void testGetValueNull() {
+	        Token tk = new Token(TokenType.DIVIDE, 6);
+	        assertNull(tk.getValue());
+	    }
+
+	    @Test
+	    void testToString() {
+	        Token token1 = new Token(TokenType.SEMI, 1);
+	        Token token2 = new Token(TokenType.ID, 2, "b");
+
+	        assertEquals("<SEMI,r:1>", token1.toString());
+	        assertEquals("<ID,r:2,b>", token2.toString());
+	    }
+
+
+	    @Test
+	    void testDifferentRows() {
+	        Token token1 = new Token(TokenType.ID, 1, "a");
+	        Token token2 = new Token(TokenType.ID, 2, "a");
+
+	        assertNotEquals(token1, token2);
+	    }
 
 }
