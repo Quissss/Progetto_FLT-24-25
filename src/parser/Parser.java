a15f549c00ad86e26f45d00f6ec28d5507c32666
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2024-12-12 03:06:56 +0100

    Aggiunte Parser
    Aggiunti e sistemate produzioni parser "PER ORA"

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -46,12 +46,13 @@ public class Parser {
 
 		switch (tk.getType()) {
 		// Prg -> DSs $
-		case TYFLOAT, TYINT, ID, PRINT, EOF -> { // Prg -> DSs $
+		case TYFLOAT, TYINT, ID, PRINT, EOF -> {
 			parseDSs();
 			match(TokenType.EOF);
 		}
-		// default SyntacticException:
-		// token tk alla riga tk.getRiga() non e’ inizio di programma
+		default -> {
+			throw new SyntacticException(tk.getRow(), "TYFLOAT, TYINT, ID, PRINT o EOF", tk.getType());
+		}
 		}
 	}
 
@@ -67,12 +68,14 @@ public class Parser {
 		switch (tk.getType()) {
 		// DSs -> Dcl DSs
 		case TYFLOAT, TYINT -> {
-
+			parseDcl();
+			parseDSs();
 		}
 
 		// DSs -> Stm DSs
 		case ID, PRINT -> {
-
+			parseStm();
+			parseDSs();
 		}
 
 		// DSs -> ϵ
@@ -80,7 +83,7 @@ public class Parser {
 
 		}
 		default -> {
-			throw new SyntacticException(tk.getRow(), "TYFLOAT, TYINT, ID, PRINT o EOF", tk.getType());
+			throw new SyntacticException(tk.getRow(), "TYFLOAT, TYINT o ID, PRINT o EOF", tk.getType());
 		}
 		}
 
@@ -98,10 +101,12 @@ public class Parser {
 		switch (tk.getType()) {
 		// Dcl -> Ty id DclP
 		case TYFLOAT, TYINT -> {
-
+			parseTy();
+			match(TokenType.ID);
+			parseDclP();
 		}
 		default -> {
-			throw new SyntacticException(tk.getRow(), "TYFLOAT o TYINT", tk.getType());
+			throw new SyntacticException(tk.getRow(), "TYFLOAT , TYINT", tk.getType());
 		}
 		}
 
@@ -152,9 +157,8 @@ public class Parser {
 		// DclP -> = Exp ;
 		case ASSIGN -> {
 			match(TokenType.ASSIGN);
-
+			parseExp();
 			match(TokenType.SEMI);
-
 		}
 		default -> {
 			throw new SyntacticException(tk.getRow(), "SEMI o OP_ASSIGN", tk.getType());
@@ -172,6 +176,28 @@ public class Parser {
 			throw new SyntacticException(e.getMessage());
 		}
 
+		switch (tk.getType()) {
+		// Stm -> id Op Exp ;
+		case ID -> {
+
+			match(TokenType.ID);
+			parseOp();
+			parseExp();
+			match(TokenType.SEMI);
+
+		}
+		// Stm -> print id ;
+		case PRINT -> {
+			match(TokenType.PRINT);
+			match(TokenType.ID);
+			match(TokenType.SEMI);
+
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "ID o PRINT", tk.getType());
+		}
+		}
+
 	}
 
 	private void parseExp() throws SyntacticException {
@@ -181,6 +207,17 @@ public class Parser {
 			tk = scanner.peekToken();
 		} catch (LexicalException e) {
 			throw new SyntacticException(e.getMessage());
+		}
+
+		switch (tk.getType()) {
+		// Exp -> Tr ExpP
+		case ID, FLOAT, INT -> {
+			parseTr();
+			parseExpP();
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "ID, FLOAT , INT", tk.getType());
+		}
 		}
 
 	}
@@ -194,6 +231,28 @@ public class Parser {
 			throw new SyntacticException(e.getMessage());
 		}
 
+		switch (tk.getType()) {
+		// Exp -> + Tr ExpP
+		case PLUS -> {
+			match(TokenType.PLUS);
+			parseTr();
+			parseExpP();
+		}
+		// Exp -> - Tr ExpP
+		case MINUS -> {
+			match(TokenType.MINUS);
+			parseTr();
+			parseExpP();
+		}
+		// Exp -> ϵ
+		case SEMI -> {
+
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "PLUS o MINUS o SEMI", tk.getType());
+		}
+		}
+
 	}
 
 	private void parseTr() throws SyntacticException {
@@ -203,6 +262,16 @@ public class Parser {
 			tk = scanner.peekToken();
 		} catch (LexicalException e) {
 			throw new SyntacticException(e.getMessage());
+		}
+		switch (tk.getType()) {
+		// Tr -> Val TrP
+		case ID, FLOAT, INT -> {
+			parseVal();
+			parseTrP();
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "ID, FLOAT, INT", tk.getType());
+		}
 		}
 
 	}
@@ -214,6 +283,27 @@ public class Parser {
 			tk = scanner.peekToken();
 		} catch (LexicalException e) {
 			throw new SyntacticException(e.getMessage());
+		}
+		switch (tk.getType()) {
+		// TrP -> * Val TrP
+		case TIMES -> {
+			match(TokenType.TIMES);
+			parseVal();
+			parseTrP();
+		}
+		// TrP -> / Val TrP
+		case DIVIDE -> {
+			match(TokenType.DIVIDE);
+			parseVal();
+			parseTrP();
+		}
+		// TrP -> ϵ
+		case MINUS, PLUS, SEMI -> {
+
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "TIMES o DIVIDE o MINUS, PLUS, SEMI", tk.getType());
+		}
 		}
 
 	}
@@ -227,6 +317,23 @@ public class Parser {
 			throw new SyntacticException(e.getMessage());
 		}
 
+		switch (tk.getType()) {
+		// Val -> intVal
+		case INT -> {
+			match(TokenType.INT);
+		}
+		// Val -> floatVal
+		case FLOAT -> {
+			match(TokenType.FLOAT);
+		}
+		// Val -> id
+		case ID -> {
+			match(TokenType.ID);
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "INT o FLOAT o ID", tk.getType());
+		}
+		}
 	}
 
 	private void parseOp() throws SyntacticException {
@@ -236,6 +343,20 @@ public class Parser {
 			tk = scanner.peekToken();
 		} catch (LexicalException e) {
 			throw new SyntacticException(e.getMessage());
+		}
+
+		switch (tk.getType()) {
+		// Op → =
+		case ASSIGN -> {
+			match(TokenType.ASSIGN);
+		}
+		// Op → opAss
+		case OP_ASSIGN -> {
+			match(TokenType.OP_ASSIGN);
+		}
+		default -> {
+			throw new SyntacticException(tk.getRow(), "ASSIGN O OP_ASSIGN", tk.getType());
+		}
 		}
 
 	}
