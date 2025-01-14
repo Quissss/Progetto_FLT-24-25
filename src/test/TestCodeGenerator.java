11a327c736fefbd9ced7ae31645dd984cf228ee7
Luca Benetti <163659760+Quissss@users.noreply.github.com>
2025-01-14 14:39:58 +0100

    Aggiunti Test Code
    CodeGenerator test in piu

---
+++ src/test/TestCodeGenerator.java
@@ -0,0 +1,69 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.Test;
+
+import ast.NodeProgram;
+import exception.SyntacticException;
+import parser.Parser;
+import scanner.Scanner;
+import visitor.CodeGeneratorVisitor;
+import visitor.TypeCheckingVisitor;
+
+class TestCodeGenerator {
+		
+	
+	@Test
+	void testAssign1() throws FileNotFoundException, SyntacticException {
+		NodeProgram nP = new Parser(new Scanner("src/test/data/TestCodeGenerator/1_assign.txt")).parse();
+		var tcVisit = new TypeCheckingVisitor();
+		nP.accept(tcVisit);
+		var cgVisit = new CodeGeneratorVisitor();
+		nP.accept(cgVisit);
+		
+		assertEquals(cgVisit.getLog(), "");
+		assertEquals(cgVisit.getCodiceGenerato(), "1 6 / sa la p P");
+	}
+	
+	@Test
+	void testDivisioni() throws FileNotFoundException, SyntacticException {
+		NodeProgram nP = new Parser(new Scanner("src/test/data/TestCodeGenerator/2_divsioni.txt")).parse();
+		var tcVisit = new TypeCheckingVisitor();
+		nP.accept(tcVisit);
+		var cgVisit = new CodeGeneratorVisitor();
+		nP.accept(cgVisit);
+		
+		
+		assertEquals(cgVisit.getLog(), "");
+		assertEquals(cgVisit.getCodiceGenerato(), "0 sa 1 sa 6 sb  1.0 6 / la lb / + sc la p P lb p P lc p P");
+	}
+	
+	@Test
+	void testGenerale() throws FileNotFoundException, SyntacticException {
+		NodeProgram nP = new Parser(new Scanner("src/test/data/TestCodeGenerator/3_generale.txt")).parse();
+		var tcVisit = new TypeCheckingVisitor();
+		nP.accept(tcVisit);
+		var cgVisit = new CodeGeneratorVisitor();
+		nP.accept(cgVisit);
+		
+		assertEquals(cgVisit.getLog(), "");
+		assertEquals(cgVisit.getCodiceGenerato(), "5 3 + sa la 0.5 + sb la p P 4 sb  lb p P lb 1 - sc lb sc lc p P");
+	}
+	
+	@Test
+	void testRegistriFiniti() throws FileNotFoundException, SyntacticException {
+		NodeProgram nP = new Parser(new Scanner("src/test/data/TestCodeGenerator/4_registriFiniti.txt")).parse();
+		var tcVisit = new TypeCheckingVisitor();
+		nP.accept(tcVisit);
+		var cgVisit = new CodeGeneratorVisitor();
+		nP.accept(cgVisit);
+		
+		/* il log se contiene un errore e non viene generato il codice */
+		assertEquals(cgVisit.getLog(), "Numero massimo di registri superato.");
+		assertEquals(cgVisit.getCodiceGenerato(), "");
+	}
+
+}
